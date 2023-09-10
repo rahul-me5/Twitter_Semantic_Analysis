92e2f0e8e032472be4bf3f86c302c08d8be3a6c2
Rahul Kumar <rahulidea146@gmail.com>
2023-09-11 02:46:24 +0530

    Twitter_Semantic_Analysis

---
+++ Streamlit_Dash.py
@@ -0,0 +1,228 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import nltk
+import re
+import string
+import base64
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load the trained model and TF-IDF vectorizer
+with open('lg_model.pkl', 'rb') as model_file, \
+     open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+    lg_model = pickle.load(model_file)
+    vectorizer = pickle.load(vectorizer_file)
+
+# Function to clean and preprocess the text
+def clean(tweet):
+
+    # Special characters
+    tweet=re.sub(r'[^a-zA-Z0-9\s]',"",tweet)
+    tweet = re.sub(r'^\d+$', "", tweet)
+    tweet = re.sub(r"\x89Û_", "", tweet)
+    tweet = re.sub(r"\x89ÛÒ", "", tweet)
+    tweet = re.sub(r"\x89ÛÓ", "", tweet)
+    tweet = re.sub(r"\x89ÛÏWhen", "When", tweet)
+    tweet = re.sub(r"\x89ÛÏ", "", tweet)
+    tweet = re.sub(r"China\x89Ûªs", "China's", tweet)
+    tweet = re.sub(r"let\x89Ûªs", "let's", tweet)
+    tweet = re.sub(r"\x89Û÷", "", tweet)
+    tweet = re.sub(r"\x89Ûª", "", tweet)
+    tweet = re.sub(r"\x89Û\x9d", "", tweet)
+    tweet = re.sub(r"å_", "", tweet)
+    tweet = re.sub(r"\x89Û¢", "", tweet)
+    tweet = re.sub(r"\x89Û¢åÊ", "", tweet)
+    tweet = re.sub(r"fromåÊwounds", "from wounds", tweet)
+    tweet = re.sub(r"åÊ", "", tweet)
+    tweet = re.sub(r"åÈ", "", tweet)
+    tweet = re.sub(r"JapÌ_n", "Japan", tweet)    
+    tweet = re.sub(r"Ì©", "e", tweet)
+    tweet = re.sub(r"å¨", "", tweet)
+    tweet = re.sub(r"SuruÌ¤", "Suruc", tweet)
+    tweet = re.sub(r"åÇ", "", tweet)
+    tweet = re.sub(r"å£3million", "3 million", tweet)
+    tweet = re.sub(r"åÀ", "", tweet)
+    
+    #emojis
+    emoji_pattern = re.compile(
+        '['
+        u'\U0001F600-\U0001F64F'  # emoticons
+        u'\U0001F300-\U0001F5FF'  # symbols & pictographs
+        u'\U0001F680-\U0001F6FF'  # transport & map symbols
+        u'\U0001F1E0-\U0001F1FF'  # flags
+        u'\U00002702-\U000027B0'
+        u'\U000024C2-\U0001F251'
+        ']+',
+        flags=re.UNICODE)
+    tweet =  emoji_pattern.sub(r'', tweet)
+    
+    # usernames mentions like "@abc123"
+    ment = re.compile(r"(@[A-Za-z0-9]+)")
+    tweet =  ment.sub(r'', tweet)
+    
+    # Contractions
+    tweet = re.sub(r"he's", "he is", tweet)
+    tweet = re.sub(r"there's", "there is", tweet)
+    tweet = re.sub(r"We're", "We are", tweet)
+    tweet = re.sub(r"That's", "That is", tweet)
+    tweet = re.sub(r"won't", "will not", tweet)
+    tweet = re.sub(r"they're", "they are", tweet)
+    tweet = re.sub(r"Can't", "Cannot", tweet)
+    tweet = re.sub(r"wasn't", "was not", tweet)
+    tweet = re.sub(r"don\x89Ûªt", "do not", tweet)
+    tweet = re.sub(r"aren't", "are not", tweet)
+    tweet = re.sub(r"isn't", "is not", tweet)
+    tweet = re.sub(r"What's", "What is", tweet)
+    tweet = re.sub(r"haven't", "have not", tweet)
+    tweet = re.sub(r"hasn't", "has not", tweet)
+    tweet = re.sub(r"There's", "There is", tweet)
+    tweet = re.sub(r"He's", "He is", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"You're", "You are", tweet)
+    tweet = re.sub(r"I'M", "I am", tweet)
+    tweet = re.sub(r"shouldn't", "should not", tweet)
+    tweet = re.sub(r"wouldn't", "would not", tweet)
+    tweet = re.sub(r"i'm", "I am", tweet)
+    tweet = re.sub(r"I\x89Ûªm", "I am", tweet)
+    tweet = re.sub(r"I'm", "I am", tweet)
+    tweet = re.sub(r"Isn't", "is not", tweet)
+    tweet = re.sub(r"Here's", "Here is", tweet)
+    tweet = re.sub(r"you've", "you have", tweet)
+    tweet = re.sub(r"you\x89Ûªve", "you have", tweet)
+    tweet = re.sub(r"we're", "we are", tweet)
+    tweet = re.sub(r"what's", "what is", tweet)
+    tweet = re.sub(r"couldn't", "could not", tweet)
+    tweet = re.sub(r"we've", "we have", tweet)
+    tweet = re.sub(r"it\x89Ûªs", "it is", tweet)
+    tweet = re.sub(r"doesn\x89Ûªt", "does not", tweet)
+    tweet = re.sub(r"It\x89Ûªs", "It is", tweet)
+    tweet = re.sub(r"Here\x89Ûªs", "Here is", tweet)
+    tweet = re.sub(r"who's", "who is", tweet)
+    tweet = re.sub(r"I\x89Ûªve", "I have", tweet)
+    tweet = re.sub(r"y'all", "you all", tweet)
+    tweet = re.sub(r"can\x89Ûªt", "cannot", tweet)
+    tweet = re.sub(r"would've", "would have", tweet)
+    tweet = re.sub(r"it'll", "it will", tweet)
+    tweet = re.sub(r"we'll", "we will", tweet)
+    tweet = re.sub(r"wouldn\x89Ûªt", "would not", tweet)
+    tweet = re.sub(r"We've", "We have", tweet)
+    tweet = re.sub(r"he'll", "he will", tweet)
+    tweet = re.sub(r"Y'all", "You all", tweet)
+    tweet = re.sub(r"Weren't", "Were not", tweet)
+    tweet = re.sub(r"Didn't", "Did not", tweet)
+    tweet = re.sub(r"they'll", "they will", tweet)
+    tweet = re.sub(r"they'd", "they would", tweet)
+    tweet = re.sub(r"DON'T", "DO NOT", tweet)
+    tweet = re.sub(r"That\x89Ûªs", "That is", tweet)
+    tweet = re.sub(r"they've", "they have", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"should've", "should have", tweet)
+    tweet = re.sub(r"You\x89Ûªre", "You are", tweet)
+    tweet = re.sub(r"where's", "where is", tweet)
+    tweet = re.sub(r"Don\x89Ûªt", "Do not", tweet)
+    tweet = re.sub(r"we'd", "we would", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"weren't", "were not", tweet)
+    tweet = re.sub(r"They're", "They are", tweet)
+    tweet = re.sub(r"Can\x89Ûªt", "Cannot", tweet)
+    tweet = re.sub(r"you\x89Ûªll", "you will", tweet)
+    tweet = re.sub(r"I\x89Ûªd", "I would", tweet)
+    tweet = re.sub(r"let's", "let us", tweet)
+    tweet = re.sub(r"it's", "it is", tweet)
+    tweet = re.sub(r"can't", "cannot", tweet)
+    tweet = re.sub(r"don't", "do not", tweet)
+    tweet = re.sub(r"you're", "you are", tweet)
+    tweet = re.sub(r"i've", "I have", tweet)
+    tweet = re.sub(r"that's", "that is", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"doesn't", "does not", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"didn't", "did not", tweet)
+    tweet = re.sub(r"ain't", "am not", tweet)
+    tweet = re.sub(r"you'll", "you will", tweet)
+    tweet = re.sub(r"I've", "I have", tweet)
+    tweet = re.sub(r"Don't", "do not", tweet)
+    tweet = re.sub(r"I'll", "I will", tweet)
+    tweet = re.sub(r"I'd", "I would", tweet)
+    tweet = re.sub(r"Let's", "Let us", tweet)
+    tweet = re.sub(r"you'd", "You would", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"Ain't", "am not", tweet)
+    tweet = re.sub(r"Haven't", "Have not", tweet)
+    tweet = re.sub(r"Could've", "Could have", tweet)
+    tweet = re.sub(r"youve", "you have", tweet)  
+    tweet = re.sub(r"donå«t", "do not", tweet)   
+            
+    # Character entity references
+    tweet = re.sub(r"&amp;", "&", tweet)
+    tweet =re.sub(r'^\d+$','',tweet)
+    # html tags
+    html = re.compile(r'<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    tweet = re.sub(html, '', tweet)
+    
+    # Urls
+    tweet = re.sub(r"https?:\/\/t.co\/[A-Za-z0-9]+", "", tweet)
+    tweet = re.sub(r'https?://\S+|www\.\S+','', tweet)
+        
+    #Punctuations and special characters
+    
+    tweet = re.sub('[%s]' % re.escape(string.punctuation),'',tweet)
+    
+    tweet = tweet.lower()
+    
+    splits = tweet.split()
+    splits = [word for word in splits if word not in set(nltk.corpus.stopwords.words('english'))]
+    tweet = ' '.join(splits)
+    
+    return tweet
+
+# Function to predict sentiment
+def predict_sentiment(text):
+    # Preprocess the text
+    cleaned_text = clean(text)
+    
+    # Transform the text using the TF-IDF vectorizer
+    tfidf_vector = vectorizer.transform([cleaned_text])
+    
+    # Predict the sentiment class
+    predicted_class = lg_model.predict(tfidf_vector)[0]
+    
+    # Map class labels to sentiment labels
+    sentiment_labels = {0: "irony", 1: "regular", 2: "sarcasm"}
+    predicted_sentiment = sentiment_labels.get(predicted_class, "Unknown")
+    
+    return predicted_sentiment
+
+# Create a Streamlit app
+def main():
+    st.title("Twitter Semantic Analysis")
+    
+    # Upload the Excel file
+    uploaded_file = st.file_uploader("Upload an Excel file", type=["xls", "xlsx"])
+    
+    if uploaded_file is not None:
+        try:
+            # Read the Excel file into a DataFrame
+            df = pd.read_excel(uploaded_file)
+            
+            # Predict sentiment and add a new column
+            df['Predicted Sentiment'] = df['tweets'].apply(predict_sentiment)
+            
+            # Display the DataFrame with predictions
+            st.write("Predicted Sentiments:")
+            st.write(df)
+            
+            # Create a downloadable link for the results
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="sentiment_results.csv">Download Results</a>'
+            st.markdown(href, unsafe_allow_html=True)
+            
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
